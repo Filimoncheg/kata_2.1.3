a9d4b78b0b7ad86dd3514631622427ab68cbf1d7
Filimoncheg <trash04@mail.ru>
2024-06-03 22:23:13 +0300

    2.1.3 HelloWorld

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+
+    private String voise;
+
+    public String getVoice() {
+        return voise;
+    }
+
+    public void setVoise(String voise) {
+        this.voise = voise;
+    }
+
+}
